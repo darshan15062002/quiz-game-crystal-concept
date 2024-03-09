548df0c6ab627cc3061040ef787f6cadd2157e1e
darshan15062002 <darshanjain15062002@gmail.com>
2024-03-09 19:59:03 +0530

    sda

--- src/pages/Single/Single.jsx
+++ src/pages/Single/Single.jsx
@@ -1,20 +1,21 @@
 
 import { useParams } from "react-router-dom";
 import Chart from "../../Components/Pages/Chart/Chart";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getStudentProfile } from "../../api/authApi";
 // import List from "../../components/table/Table";
 
 const Single = () => {
     const { id } = useParams()
-
+    const [student, setStudent] = useState({})
     console.log(id);
 
 
     useEffect(() => {
 
         getStudentProfile(id).then((data) => {
-            console.log(data, "Student Profile");
+            // console.log(data.user, "Student Profile");
+            setStudent(data.user)
         })
     }, [])
     return (
@@ -24,7 +25,7 @@ const Single = () => {
         <div className="flex-grow bg-gray-100 p-4">
 
 
-            <div className="flex gap-4 p-4">
+            <div className="flex flex-col mt-16 gap-4 p-4">
                 <div className="flex-1 bg-white p-4 shadow-md relative">
                     <div className="absolute top-0 right-0 p-2">
                         <button className="text-blue-500 bg-blue-100 px-2 py-1 text-sm rounded-tr-md cursor-pointer">
@@ -32,33 +33,30 @@ const Single = () => {
                         </button>
                     </div>
 
-                    <div className="flex gap-4 items-center">
-                        <img
+                    <div className="flex pl-5 gap-4 items-center">
+                        {/* <img
                             src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
                             alt=""
                             className="w-20 h-20 rounded-full"
-                        />
+                        /> */}
 
                         <div className="flex flex-col">
-                            <h1 className="text-xl font-bold">Jane Doe</h1>
+                            <h1 className="text-xl font-bold">{student.name}</h1>
                             <div className="flex flex-col mt-2">
-                                <div className="flex items-center">
-                                    <span className="font-bold text-gray-500">Email:</span>
-                                    <span className="ml-2">janedoe@gmail.com</span>
-                                </div>
+
                                 <div className="flex items-center">
                                     <span className="font-bold text-gray-500">Phone:</span>
-                                    <span className="ml-2">+1 2345 67 89</span>
+                                    <span className="ml-2">{student.phone}</span>
                                 </div>
                                 <div className="flex items-center">
                                     <span className="font-bold text-gray-500">Address:</span>
                                     <span className="ml-2">
-                                        Elton St. 234 Garden Yd. NewYork
+                                        {student.location}
                                     </span>
                                 </div>
                                 <div className="flex items-center">
-                                    <span className="font-bold text-gray-500">Country:</span>
-                                    <span className="ml-2">USA</span>
+                                    <span className="font-bold text-gray-500">Standard:</span>
+                                    <span className="ml-2">{student.std}</span>
                                 </div>
                             </div>
                         </div>
