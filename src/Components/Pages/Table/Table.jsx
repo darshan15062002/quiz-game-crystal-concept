73166ec28ea64ffbb65dc0c4a00b9fce71d7898a
Darshan jain <darshanjain15062002@gmail.com>
2024-11-23 09:46:05 +0530

    dsfsdfsd

--- src/Components/Pages/Table/Table.jsx
+++ src/Components/Pages/Table/Table.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-
-export const Table = ({ users }) => {
+export const Table = ({ users, page = 1 }) => {
+    const startNumber = (page - 1) * 10 + 1;
 
     return (
-
-        <div className=" bg-white overflow-x-scroll px-4  no-scrollbar p-5 rounded-lg shadow-lg ">
-            <table className=' w-full overflow-x-scroll  border '>
-                <tr className='text-black  '>
-                    <th className='border-r-2 px-3 py-2  border-b '>No.</th>
-                    <th className='border-r-2 px-3 py-2  border-b'>Name</th>
-                    <th className='border-r-2 px-3 py-2  border-b'>Phone</th>
-                    <th className='border-r-2 px-3 py-2  border-b'>Std</th>
-                    <th className='border-r-2 px-3 py-2  border-b'>Location</th>
-
-
-
-
-                </tr>
-                {users?.map((user, index) => (
-                    <tr className='text-gray-500 w-full '>
-                        <th className='border-r-2 px-3 py-2  border-b  text-center  '>{index + 1}</th>
-                        <td className='border-r-2 px-3 py-2  border-b  text-center '>{user?.name}</td>
-                        <td className='border-r-2 px-3 py-2  border-b  text-center '>{user?.phone}</td>
-                        <td className='border-r-2 px-3 py-2  border-b  text-center '>{user.std}</td>
-                        <td className='border-r-2 px-3 py-2  border-b  text-center '>{user.location}</td>
+        <div className="bg-white overflow-x-scroll px-4 no-scrollbar p-5 rounded-lg shadow-lg">
+            <table className='w-full overflow-x-scroll border'>
+                <thead>
+                    <tr className='text-black'>
+                        <th className='border-r-2 px-3 py-2 border-b'>No.</th>
+                        <th className='border-r-2 px-3 py-2 border-b'>Name</th>
+                        <th className='border-r-2 px-3 py-2 border-b'>Phone</th>
+                        <th className='border-r-2 px-3 py-2 border-b'>Std</th>
+                        <th className='border-r-2 px-3 py-2 border-b'>Location</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {users?.map((user, index) => (
+                        <tr key={user._id} className='text-gray-500 w-full'>
+                            <th className='border-r-2 px-3 py-2 border-b text-center'>
+                                {startNumber + index}
+                            </th>
+                            <td className='border-r-2 px-3 py-2 border-b text-center'>{user?.name}</td>
+                            <td className='border-r-2 px-3 py-2 border-b text-center'>{user?.phone}</td>
+                            <td className='border-r-2 px-3 py-2 border-b text-center'>{user.std}</td>
+                            <td className='border-r-2 px-3 py-2 border-b text-center'>{user.location}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-
-
-
         </div>
-
     )
 }
